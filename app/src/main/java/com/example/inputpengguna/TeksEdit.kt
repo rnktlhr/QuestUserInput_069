2fef9916cdfb70d21586a1924a77fc1fe496f3b7
rnktlhr <rinakitluhur10@gmail.com>
2025-10-30 11:38:45 +0700

    menambahkan var text NAMA pada fungsi fromdata diri di teks edit

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,9 +1,15 @@
 package com.example.inputpengguna
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
-fun FormDataDiri(
+fun FormDataDiri(modifier: Modifier
 
-){}
+){
+    var textNama by remember { mutableStateOf("") }
+}
