ad721669ef3fc9ec60dda9fa8fbb49f15d633f9e
rnktlhr <rinakitluhur10@gmail.com>
2025-10-30 11:46:21 +0700

    menambahkan colom pada fungsi fromdata diri di teks edit

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,11 +1,15 @@
 package com.example.inputpengguna
 
+import android.R.attr.top
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -20,4 +24,6 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember { mutableStateOf("") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier= Modifier.padding(top = 50.dp)),
 }
