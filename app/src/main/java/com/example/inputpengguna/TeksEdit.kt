986b9eeb28cccd33b762970f3299107cc99dbfd4
rnktlhr <rinakitluhur10@gmail.com>
2025-10-30 11:53:48 +0700

    menambahkan text field pada coloum pada fungsi fromdata diri di teks edit

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,13 +1,20 @@
 package com.example.inputpengguna
 
 import android.R.attr.top
+import android.graphics.Outline
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -25,5 +32,21 @@ fun FormDataDiri(modifier: Modifier
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
 
-    Column(modifier= Modifier.padding(top = 50.dp)),
+    Column(modifier= Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = {Text(text = "Nama Lengkap")},
+            onValueChange = {
+                textNama = it
+            }
+        )
+        }
+
+
+
 }
