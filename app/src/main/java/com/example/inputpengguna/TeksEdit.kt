a25f000a486d2efc377bda89d3080800edbc4d0f
rnktlhr <rinakitluhur10@gmail.com>
2025-10-30 11:43:40 +0700

    menambahkan val gender pada fungsi fromdata diri di teks edit

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -18,4 +18,6 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki", "Perempuan")
 }
